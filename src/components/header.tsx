6ad8cd02dddc8abe95038a0366fb19e0bfe75353
Felipe Cassano <felipecassanoa@gmail.com>
2025-04-14 19:49:42 -0300

    Melhorias no header-mobile e no contact

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,18 +1,42 @@
 'use client';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import BrsfLogo from '../imgs/BRSF.avif'; // Importando a imagem do logo
 import Image from 'next/image';
 
 function Header() {
+  useEffect(() => {
+    const mobileButton = document.getElementById('mobile-button');
+    const mobileMenu = document.getElementById('mobile-menu');
+
+    const handleButtonClick = () => {
+      mobileMenu?.classList.toggle('hidden');
+      mobileMenu?.classList.toggle('flex');
+    };
+
+    const handleMenuClick = () => {
+      mobileMenu?.classList.add('hidden');
+      mobileMenu?.classList.remove('flex');
+      setIsMenuOpen(!isMenuOpen);
+    };
+
+    mobileButton?.addEventListener('click', handleButtonClick);
+    mobileMenu?.addEventListener('click', handleMenuClick);
+
+    // Cleanup event listeners on component unmount
+    return () => {
+      mobileButton?.removeEventListener('click', handleButtonClick);
+      mobileMenu?.removeEventListener('click', handleMenuClick);
+    };
+  }, []);
+
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
-  const navItemClasses = 'flex flex-col items-center justify-center gap-1 ';
-  const navLinkClasses = 'md:underline-offset-6 md:hover:underline';
-  const navSeparatorClasses =
-    'max-md:w-40 max-md:border-b-1 max-md:border-zinc-500';
+  const navItemClasses = 'animate-slide-in-scale opacity-0';
+  const navLinkClasses =
+    'block p-4 max-lg:hover:bg-white/10 lg:px-0 lg:py-2 lg:underline-offset-8 lg:hover:underline';
 
   return (
     <header className='flex items-center justify-between bg-zinc-200 p-8'>
@@ -28,24 +52,14 @@ function Header() {
       </a>
 
       <nav
-        className={`z-40 flex items-center max-md:fixed max-md:inset-0 max-md:w-full md:block ${
-          isMenuOpen ? 'max-md:visible' : 'max-md:invisible'
-        }`}
+        className='z-40 hidden items-center max-lg:fixed max-lg:inset-0 max-lg:w-full lg:block'
         id='mobile-menu'
       >
-        <div
-          className={`fixed inset-0 bg-white backdrop-blur-sm duration-200 ease-in-out ${
-            isMenuOpen
-              ? 'pointer-events-auto opacity-90'
-              : 'pointer-events-none opacity-0'
-          }`}
-        ></div>
+        <div className='fixed inset-0 bg-zinc-200/10 backdrop-blur-lg duration-200 ease-in-out lg:hidden'></div>
         {/* Close button in top-right corner */}
         <button
           onClick={toggleMenu}
-          className={`absolute top-11 right-12 z-50 flex h-8 w-8 items-center justify-center rounded-full bg-zinc-100 transition-opacity duration-300 md:hidden ${
-            isMenuOpen ? 'opacity-100' : 'pointer-events-none opacity-0'
-          }`}
+          className='absolute top-11 right-12 z-50 flex h-8 w-8 items-center justify-center rounded-full bg-zinc-100 transition-opacity duration-300 lg:hidden'
           aria-label='Fechar menu'
         >
           <span className='relative flex h-5 w-5'>
@@ -54,41 +68,28 @@ function Header() {
           </span>
         </button>
 
-        <ul
-          className={`flex list-none gap-12 font-sans text-[14px] uppercase transition-all duration-300 ease-in-out max-md:absolute max-md:w-full max-md:flex-col max-md:justify-center max-md:gap-2 max-md:p-10 max-md:text-center *:max-md:text-[20px] md:flex ${
-            isMenuOpen
-              ? 'max-md:flex max-md:translate-y-0 max-md:opacity-100'
-              : 'max-md:hidden'
-          } `}
-        >
-          <li className={navItemClasses}>
-            <a
-              className='underline-offset-6 max-md:text-blue-400 md:hover:underline'
-              href='#inicio'
-            >
+        <ul className='flex list-none gap-10 font-sans uppercase transition-all max-lg:absolute max-lg:z-50 max-lg:w-full max-lg:flex-col max-lg:justify-center max-lg:gap-2 max-lg:divide-y-2 max-lg:divide-black/40 max-lg:p-10 max-lg:text-center *:max-lg:text-[20px] lg:flex lg:text-[15px]'>
+          <li className={navItemClasses} style={{ animationDelay: '100ms' }}>
+            <a className={navLinkClasses} href='#inicio'>
               Início
             </a>
-            <div className={navSeparatorClasses}></div>
           </li>
-          <li className={navItemClasses}>
+          <li className={navItemClasses} style={{ animationDelay: '200ms' }}>
             <a className={navLinkClasses} href='#quem-somos'>
               quem somos
             </a>
-            <div className={navSeparatorClasses}></div>
           </li>
-          <li className={navItemClasses}>
+          <li className={navItemClasses} style={{ animationDelay: '300ms' }}>
             <a className={navLinkClasses} href='#missao'>
               Missão
             </a>
-            <div className={navSeparatorClasses}></div>
           </li>
-          <li className={navItemClasses}>
+          <li className={navItemClasses} style={{ animationDelay: '400ms' }}>
             <a className={navLinkClasses} href='#clientes'>
               Clientes
             </a>
-            <div className={navSeparatorClasses}></div>
           </li>
-          <li>
+          <li className={navItemClasses} style={{ animationDelay: '500ms' }}>
             <a className={navLinkClasses} href='#contato'>
               Contato
             </a>
@@ -97,11 +98,12 @@ function Header() {
       </nav>
 
       <button
-        className='text-verde-900 mouser-pointer flex cursor-pointer items-center justify-center gap-3 rounded-full bg-zinc-200 px-4 py-2 md:hidden'
+        className='mouser-pointer flex cursor-pointer items-center justify-center gap-3 rounded-full bg-zinc-200 px-4 py-2 text-zinc-900 lg:hidden'
         onClick={toggleMenu}
+        id='mobile-button'
       >
         <p>Menu</p>
-        <span className='flex h-3 w-5 flex-col justify-between *:h-0.5 *:rounded-md *:bg-black'>
+        <span className='flex h-3 w-5 flex-col justify-between *:h-0.5 *:rounded-lg *:bg-black'>
           <span
             className={`transition-transform duration-300 ${
               isMenuOpen ? 'translate-y-1 rotate-45' : ''
